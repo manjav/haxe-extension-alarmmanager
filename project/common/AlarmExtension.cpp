30042f2caf183d8a76633370c458eea8eadbda4e
Mansour Djawadi <mansurjavadi@gmail.com>
2017-04-02 19:45:13 +0430

    added lime classes and build catch

---
+++ project/common/AlarmExtension.cpp
@@ -0,0 +1,14 @@
+#include "Utils.h"
+
+
+namespace alarmextension {
+	
+	
+	int SampleMethod(int inputValue) {
+		
+		return inputValue * 100;
+		
+	}
+	
+	
+}
---
+++ project/include/Utils.h
@@ -0,0 +1,12 @@
+#ifndef ALARMEXTENSION_H
+#define ALARMEXTENSION_H
+
+namespace alarmextension {
+	
+	
+	int SampleMethod(int inputValue);
+	
+	
+}
+
+#endif
